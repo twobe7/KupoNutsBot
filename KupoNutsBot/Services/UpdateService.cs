9b9196407d3e4a12f935c3965d6548e5ee578a16
Yuuki <Yuuki_Minoru@live.com.au>
2019-07-29 21:04:08 +1000

    Update UpdateService.cs

--- KupoNutsBot/Services/UpdateService.cs
+++ KupoNutsBot/Services/UpdateService.cs
@@ -10,6 +10,8 @@ namespace KupoNutsBot.Services
 
 	public class UpdateService : ServiceBase
 	{
+		private static string updateFile = "update.sh";
+
 		public override Task Initialize()
 		{
 			CommandsService.BindCommand("update", this.Update);
@@ -24,16 +26,25 @@ namespace KupoNutsBot.Services
 
 		private async Task Update(string[] args, SocketMessage message)
 		{
+			Log.Write("Begining Update");
+
 			string content = "echo Kupo Nuts Bot Update Script";
 			content += @"sleep 5s";
 			content += @"git -C KupoNutsBot/ pull origin master";
 			content += @"dotnet build KupoNutsBot/KupoNutsBot.sln";
 			content += @"dotnet KupoNutsBot/KupoNutsBot/bin/KupoNutsBot.dll";
 
-			File.WriteAllText("update.sh", content);
+			Log.Write("Writing update script");
 
-			await BashUtils.Run("chmod +x update.sh");
-			await BashUtils.Run("sh update.sh", false);
+			if (File.Exists(updateFile))
+				File.Delete(updateFile);
+
+			File.WriteAllText(updateFile, content);
+
+			Log.Write("Running update script");
+
+			await BashUtils.Run("chmod +x " + updateFile);
+			await BashUtils.Run("sh " + updateFile, false);
 			await Program.Exit();
 		}
 	}
