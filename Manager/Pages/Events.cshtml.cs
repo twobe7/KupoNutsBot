9b608e45d8d33e35e31861a9bf0cc05d15676f6b
Yuuki <yuuki.walsh@outlook.com>
2019-07-31 23:42:13 +1000

    create event modal dialog

--- Manager/Pages/Events.cshtml.cs
+++ Manager/Pages/Events.cshtml.cs
@@ -6,6 +6,7 @@ namespace Manager.Pages
 	using System.Collections.Generic;
 	using System.Linq;
 	using System.Threading.Tasks;
+	using KupoNutsBot.Events;
 	using Microsoft.AspNetCore.Mvc;
 	using Microsoft.AspNetCore.Mvc.RazorPages;
 	using Microsoft.AspNetCore.Mvc.Rendering;
@@ -14,15 +15,35 @@ namespace Manager.Pages
 	{
 		public SelectList ChannelList { get; set; }
 
+		public SelectList ColorList { get; set; }
+
+		public DateTime DropOffDate { get; set; }
+
 		public void OnGet()
 		{
 			List<string> values = new List<string>();
 			values.Add("Test");
 
 			this.ChannelList = new SelectList(values);
+
+			this.ColorList = new SelectList(Enum.GetNames(typeof(Event.Colors)));
 		}
 
-		public void OnPostEvent(int id)
+		public void OnPostEvent(
+			string eventName,
+			string eventDesc,
+			string channelSelect,
+			Event.Colors colorSelect,
+			DateTime eventDate,
+			TimeSpan eventTime,
+			TimeSpan eventDuration,
+			bool repeatMonday,
+			bool repeatTuesday,
+			bool repeatWednesday,
+			bool repeatThursday,
+			bool repeatFriday,
+			bool repeatSaturday,
+			bool repeatSunday)
 		{
 			Console.WriteLine("Post used");
 		}
