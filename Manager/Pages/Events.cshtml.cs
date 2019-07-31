d17afe62fa1850c1f86e7aec0317b6dea194bd1e
Yuuki <yuuki.walsh@outlook.com>
2019-07-31 15:32:57 +1000

    Initial managment ASP.NET razor pages

---
+++ Manager/Pages/Events.cshtml.cs
@@ -0,0 +1,18 @@
+﻿// This document is intended for use by Kupo Nut Brigade developers.
+
+namespace Manager.Pages
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Threading.Tasks;
+	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.AspNetCore.Mvc.RazorPages;
+
+	public class Events : PageModel
+	{
+		public void OnGet()
+		{
+		}
+	}
+}
