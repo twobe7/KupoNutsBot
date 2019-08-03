bcee774be6ab7aba2c4423a4c2185b4d3353b6c5
Yuuki <yuuki.walsh@outlook.com>
2019-08-03 19:44:41 +1000

    remove debug commands

--- KupoNuts.Bot/Services/DebugService.cs
+++ KupoNuts.Bot/Services/DebugService.cs
@@ -14,16 +14,12 @@ namespace KupoNuts.Bot.Services
 		public override Task Initialize()
 		{
 			CommandsService.BindCommand("time", this.Time, Permissions.Everyone, "Shows the current time in multiple time zones.");
-			CommandsService.BindCommand("notImplemented", this.Test, Permissions.Administrators, "Tests NotImplementedException handler.");
-			CommandsService.BindCommand("error", this.Error, Permissions.Administrators, "Tests generic Exception handler.");
 			return Task.CompletedTask;
 		}
 
 		public override Task Shutdown()
 		{
 			CommandsService.ClearCommand("time");
-			CommandsService.ClearCommand("notImplemented");
-			CommandsService.ClearCommand("error");
 			return Task.CompletedTask;
 		}
 
@@ -31,16 +27,6 @@ namespace KupoNuts.Bot.Services
 		{
 			Instant now = SystemClock.Instance.GetCurrentInstant();
 			await message.Channel.SendMessageAsync("The time is: " + TimeUtils.GetDateTimeString(now));
-		}
-
-		private Task Test(string[] args, SocketMessage message)
-		{
-			throw new NotImplementedException();
-		}
-
-		private Task Error(string[] args, SocketMessage message)
-		{
-			throw new Exception("This is a test error. Nothing has gone wrong.");
 		}
 	}
 }
