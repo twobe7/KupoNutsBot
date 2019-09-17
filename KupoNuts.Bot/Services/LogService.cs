176d0a900a38a0ddfbb4b8267e042dc1a199cebf
Yuuki <yuuki.walsh@outlook.com>
2019-09-17 17:16:11 +1000

    dump log to discord

--- KupoNuts.Bot/Services/LogService.cs
+++ KupoNuts.Bot/Services/LogService.cs
@@ -13,8 +13,13 @@ namespace KupoNuts.Bot.Services
 
 	public class LogService : ServiceBase
 	{
+		private Queue<string> logMessages = new Queue<string>();
+
 		public override Task Initialize()
 		{
+			Log.MessageLogged += this.OnMessageLogged;
+			Log.ExceptionLogged += this.OnExceptionLogged;
+
 			Program.DiscordClient.UserJoined += this.DiscordClient_UserJoined;
 			Program.DiscordClient.UserLeft += this.DiscordClient_UserLeft;
 			Program.DiscordClient.UserBanned += this.DiscordClient_UserBanned;
@@ -37,6 +42,21 @@ namespace KupoNuts.Bot.Services
 		public async Task LogMe(SocketMessage message)
 		{
 			await this.PostMessage((SocketGuildUser)message.Author, Color.Purple, "Is testing");
+		}
+
+		[Command("Log", Permissions.Administrators, "posts the bot log")]
+		public async Task PostLog(SocketMessage message)
+		{
+			StringBuilder text = new StringBuilder();
+
+			foreach (string msg in this.logMessages)
+				text.AppendLine(msg);
+
+			EmbedBuilder builder = new EmbedBuilder();
+			builder.Title = "Log" + " [" + DateTime.Now.ToString("HH:mm:ss") + "]";
+			builder.Description = text.ToString();
+
+			await message.Channel.SendMessageAsync(null, false, builder.Build());
 		}
 
 		private async Task DiscordClient_UserJoined(SocketGuildUser user)
@@ -87,6 +107,21 @@ namespace KupoNuts.Bot.Services
 			builder.Footer.Text = "ID: " + user.Id;
 
 			await channel.SendMessageAsync(null, false, builder.Build());
+		}
+
+		private void OnExceptionLogged(string str)
+		{
+			this.OnMessageLogged(str);
+		}
+
+		private void OnMessageLogged(string str)
+		{
+			this.logMessages.Enqueue(str);
+
+			while (this.logMessages.Count > 100)
+			{
+				this.logMessages.Dequeue();
+			}
 		}
 	}
 }
