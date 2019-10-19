f7b47f565372191c18100f7fd67f759dc994c90c
Yuuki <yuuki.walsh@outlook.com>
2019-10-19 21:33:46 +1100

    path utils

--- KupoNuts.Bot/Characters/PortraitDrawer.cs
+++ KupoNuts.Bot/Characters/PortraitDrawer.cs
@@ -5,6 +5,7 @@ namespace KupoNuts.Bot.Characters
 	using System.IO;
 	using System.Threading.Tasks;
 	using KupoNuts.Bot.Utils;
+	using KupoNuts.Utils;
 	using SixLabors.Fonts;
 	using SixLabors.ImageSharp;
 	using SixLabors.ImageSharp.PixelFormats;
@@ -22,18 +23,18 @@ namespace KupoNuts.Bot.Characters
 		};
 
 		private static FontCollection fonts = new FontCollection();
-		private static FontFamily axisRegular = fonts.Install("./Assets/Axis-Regular.ttf");
-		private static FontFamily optimusPrincepsSemiBold = fonts.Install("./Assets/OptimusPrincepsSemiBold.ttf");
+		private static FontFamily axisRegular = fonts.Install(PathUtils.Current + "/Assets/Axis-Regular.ttf");
+		private static FontFamily optimusPrincepsSemiBold = fonts.Install(PathUtils.Current + "/Assets/OptimusPrincepsSemiBold.ttf");
 
 		public static async Task<string> Draw(Character character)
 		{
-			string portraitPath = "./Temp/" + character.ID + ".jpg";
+			string portraitPath = PathUtils.Current + "/Temp/" + character.ID + ".jpg";
 			await FileDownloader.Download(character.Portrait, portraitPath);
 
 			Image<Rgba32> charImg = Image.Load<Rgba32>(portraitPath);
 			charImg.Mutate(x => x.Resize(375, 512));
 
-			Image<Rgba32> overlayImg = Image.Load<Rgba32>("./Assets/overlay.png");
+			Image<Rgba32> overlayImg = Image.Load<Rgba32>(PathUtils.Current + "/Assets/overlay.png");
 
 			Image<Rgba32> finalImg = new Image<Rgba32>(1024, 512);
 			finalImg.Mutate(x => x.DrawImage(charImg, 1.0f));
@@ -41,7 +42,7 @@ namespace KupoNuts.Bot.Characters
 			finalImg.Mutate(x => x.DrawText(centerText, character.Name, optimusPrincepsSemiBold.CreateFont(45), Color.White, new PointF(768, 40)));
 			finalImg.Mutate(x => x.DrawText(centerText, character.Title?.Name, axisRegular.CreateFont(25), Color.White, new PointF(768, 80)));
 
-			string outputPath = "./Temp/" + character.ID + "_render.jpg";
+			string outputPath = PathUtils.Current + "/Temp/" + character.ID + "_render.jpg";
 			finalImg.Save(outputPath);
 
 			charImg.Dispose();
---
+++ KupoNuts.Shared/Utils/PathUtils.cs
@@ -0,0 +1,13 @@
+﻿// This document is intended for use by Kupo Nut Brigade developers.
+
+namespace KupoNuts.Utils
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+
+	public static class PathUtils
+	{
+		public static string Current = "./KupoNutsBot/bin/";
+	}
+}
