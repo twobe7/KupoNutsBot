fb10993230a4e795cf5806ac3db74dc65828b595
Yuuki <yuuki.walsh@outlook.com>
2020-01-11 23:16:11 +1100

    Update EventsService.cs

--- FC.Manager.Server/Services/EventsService.cs
+++ FC.Manager.Server/Services/EventsService.cs
@@ -8,6 +8,7 @@ namespace FC.Manager.Server.Services
 	using System.Collections.Generic;
 	using System.Linq;
 	using System.Threading.Tasks;
+	using FC.Data;
 	using FC.Events;
 	using FC.Manager.Server.RPC;
 
@@ -16,8 +17,12 @@ namespace FC.Manager.Server.Services
 		[GuildRpc]
 		public async Task<List<Event>> GetEvents(string guildId)
 		{
-			await Task.Delay(0);
-			return null;
+			Table<Event> eventsDb = Table<Event>.Create("KupoNuts_Events", 1);
+			await eventsDb.Connect();
+
+			Dictionary<string, object> search = new Dictionary<string, object>();
+			search.Add("ServerIdStr", guildId);
+			return await eventsDb.LoadAll(search);
 		}
 	}
 }
